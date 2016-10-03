9b4885d2af3191c5eccb9ae1694d279515af48ac
agarridogarcia <agarridogarcia@atanasoff30.danielcastelao.org>
2016-10-03 10:04:43 +0200

    millas marinas a metros

---
+++ src/boletin2_5/Boletin2_5.java
@@ -0,0 +1,19 @@
+
+package boletin2_5;
+
+import javax.swing.JOptionPane;
+
+
+public class Boletin2_5 {
+
+ 
+    public static void main(String[] args) {
+        float millasmarinas, metros;
+        int CONSTANTE= 1852;
+        millasmarinas=Float.parseFloat(JOptionPane.showInputDialog("millasmarinas"));
+                metros=millasmarinas*CONSTANTE;
+                
+                JOptionPane.showMessageDialog(null, "metros="+metros);
+    }
+    
+}
